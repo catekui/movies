f1fb82c8d5f7486785235ff3d0c78bd6e61b274d
catekui <catherine.wanjiku@student.moringaschool.com>
2022-06-27 14:16:43 +0300

    added component auth and movies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { MoviesComponent } from './movies/movies.component';
+import { AuthComponent } from './auth/auth.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    MoviesComponent,
+    AuthComponent
   ],
   imports: [
     BrowserModule,
